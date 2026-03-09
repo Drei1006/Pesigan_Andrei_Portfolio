f3b5b5ea45694aadff4fd90c265e9f340c9194fc
Andrei Pesigan <pesigandrei1006@gmail.com>
2026-03-09 15:02:43 +0800

    Updated portfolio layout

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -58,9 +58,10 @@ export function AboutSection() {
               <div className="mt-6 flex flex-wrap gap-3">
                 <Button variant="outline" size="sm" asChild className="gap-2">
                   <a
-                    href="https://drive.google.com/file/d/18U6MsChm_7_x0VvBgu5iarCLsKKfFAEi/view?usp=sharing"
+                    href="/Pesigan_Andrei_Christopher_L._Resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
+                    download
                   >
                     <FileText className="h-4 w-4" />
                     Resume
